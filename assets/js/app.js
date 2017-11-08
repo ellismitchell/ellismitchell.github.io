3b0b7f2b23d1662defe158df596c3f625d906329
ellismitchell <elligmitchell@gmail.com>
2017-11-08 11:26:39 -0800

    updated portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,56 @@
-// sanity check
 $(document).ready(function(){
 
-console.log("app.js linked.");
-
-let projects = [
-	{
-		title: "Wallingford Symphony Orchestra",
-		description: "blah blah blah",
-		picture: 'assets/imgs/Screenshot 2017-08-20 at 7.17.18 PM.png',
-		link: 'walllingfordsymphony.org'
-	},
-	{
-		title: "Eliad Design",
-		description: "blah blah blah",
-		picture: 'assets/imgs/Screenshot 2017-08-20 at 7.42.25 PM.png',
-		link: 'eliaddesign.com'
-	},
-	{
-		title: "Edward Mitchell Architects",
-		description: "blah blah blah",
-		picture: 'assets/imgs/Screenshot 2017-08-20 at 7.43.35 PM.png',
-		link: 'emarchictects.net'
-	},
-]
-for (let i = 0; i < projects.length; i++){
-$('#work').append(`<div class='col s4'><div class="card small">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="${projects[i].picture}">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">${projects[i].title}</span>
-      <p><a href="#">${projects[i].link}</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">${projects[i].title}<i class="material-icons right">close</i></span>
-      <p>${projects[i].description}</p>
-    </div>
-  </div></div>`);
-}
-// console.log(myHTML);
+	let projects = [
+		{
+			title: "ChessPrep",
+			description: "",
+			picture: 'assets/imgs/ChessPrep.png',
+			link: 'nameless-gorge-60483.herokuapp.com'
+		},
+		{
+			title: "Fantasy Football",
+			description: "",
+			picture: 'assets/imgs/Fantasy.png',
+			link: 'quiet-caverns-35726.herokuapp.com'
+		},
+		{
+			title: "Tower Defense",
+			description: "",
+			picture: 'assets/imgs/Tower_Def.png',
+			link: 'mighty-island-51639.herokuapp.com'
+		},
+		{
+			title: "Wallingford Symphony Orchestra",
+			description: "",
+			picture: 'assets/imgs/WSO.png',
+			link: 'walllingfordsymphony.org'
+		},
+		{
+			title: "Eliad Design",
+			description: "",
+			picture: 'assets/imgs/Eliad.png',
+			link: 'eliaddesign.com'
+		},
+		{
+			title: "Edward Mitchell Architects",
+			description: "",
+			picture: 'assets/imgs/EMA.png',
+			link: 'emarchictects.net'
+		},	
+	]
+	for (let i = 0; i < projects.length; i++){
+	$('#work').append(`<div class='col s4'><div class="card small hoverable">
+	    <div class="card-image waves-effect waves-block waves-light">
+	      <img class="activator" src="${projects[i].picture}">
+	    </div>
+	    <div class="card-content">
+	      <span class="card-title activator grey-text text-darken-4">${projects[i].title}</span>
+	    </div>
+	    <div class="card-reveal">
+	      <span class="card-title grey-text text-darken-4">${projects[i].title}<i class="material-icons right">close</i></span>
+	      <p>${projects[i].description}</p>
+	      <p><a href="//www.${projects[i].link}">${projects[i].link}</a></p>
+	    </div>
+	  </div></div>`);
+	}
 });
